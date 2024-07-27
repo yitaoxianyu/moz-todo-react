2db73b7ec3c8b2d4feed81976bacc6a3cefb127b
XianYu <2861587619@qq.com>
2024-07-27 16:58:08 +0800

    First Commit

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function Todo(props){
+    return (
+        <li className="todo stack-small">
+      <div className="c-cb">
+        <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+        <label className="todo-label" htmlFor="todo-0">
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">Eat</span>
+        </button>
+        <button type="button" className="btn btn__danger">
+          Delete <span className="visually-hidden">Eat</span>
+        </button>
+      </div>
+    </li>
+    );
+}
+
+export default Todo;
