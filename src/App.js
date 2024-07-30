20b4f560f677fc287a25426aac74d4f2f4956b00
XianYu <2861587619@qq.com>
2024-07-30 18:22:13 +0800

    CRUD

--- src/App.js
+++ src/App.js
@@ -3,13 +3,59 @@ import React from "react";
 import FilterButton from "./components/FilterButton.js";
 import Form from "./components/Form.js";
 import { useState } from "react";
+import {nanoid} from "nanoid";
+
 
 function App(props) {
-  const taskList = props.tasks.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>)
+  const [tasks,setTasks] = useState([]);
+  
   function Addtask(name){
-    alert(name);
+    const newTask = {id: `todo-${nanoid()}`,completed:false,name:name};
+    setTasks([...tasks,newTask]);
   }
   
+  function deleteTask(id) {
+    const remainingTask = tasks.filter((task)=> id != task.id);
+    setTasks(remainingTask);
+  }
+
+  function editTask(id, newName){
+    const editedTaskList = tasks.map((task)=>{
+      if(id == task.id){
+        return {...task,name : newName}
+      }
+      return task
+    }
+   )
+   setTasks(editedTaskList);
+  }
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map((task)=> {
+      if(task.id == id){
+        //进行修改
+        return {...task,completed:!task.completed};
+      }
+      return task;
+    }
+  )
+  setTasks(updatedTasks);
+  }
+
+  const taskList = tasks.map(
+    (task) => <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed} 
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+    editTask={editTask}
+    />
+  )
+  const tasksNoun = tasks.length >1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return(
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -19,7 +65,7 @@ function App(props) {
       <FilterButton />
       <FilterButton />  
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/index.js
+++ src/index.js
@@ -4,16 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
-];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App tasks={DATA}/>
+    <App />
   </React.StrictMode>
 );
 
